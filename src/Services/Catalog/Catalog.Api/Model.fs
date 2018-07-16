d872b02d2e3beb5c0533ea1a351a3b0984ada01d
Dominik Kotecki <dominikus1910@gmail.com>
2018-07-16 20:15:43 +0200

    add ProductDetails

--- src/Services/Catalog/Catalog.Api/Model.fs
+++ src/Services/Catalog/Catalog.Api/Model.fs
@@ -5,9 +5,15 @@ open MongoDB.Bson.Serialization.Attributes
 open MongoDB.Bson
 
 [<CLIMutable>]
+[<BsonIgnoreExtraElements>]
+type ProductDetails = { [<BsonElement>]  Weight: float; [<BsonElement>] WeightUnits: string; [<BsonElement>] Manufacturer: string; [<BsonElement>] Color: string }
+
+[<CLIMutable>]
 type Product = { [<BsonId>] [<BsonRepresentation(BsonType.String)>] Id: Guid;
                  [<BsonElement>] Slug: string;
                  [<BsonElement>] Name: string;
+                 [<BsonElement>] Description: string;
+                 [<BsonElement>] Details: ProductDetails
                  [<BsonElement>] Price: decimal
                  [<BsonElement>] AvailableStock: int
                  [<BsonElement>] PictureUri: string }
