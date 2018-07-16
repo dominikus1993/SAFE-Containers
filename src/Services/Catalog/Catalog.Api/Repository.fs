d9288c5492e81e9467bc4879dcead3eb3ffb1036
Dominik Kotecki <dominikus1910@gmail.com>
2018-07-16 20:18:37 +0200

    small change

--- src/Services/Catalog/Catalog.Api/Repository.fs
+++ src/Services/Catalog/Catalog.Api/Repository.fs
@@ -2,6 +2,18 @@ namespace Catalog.Api.Repositories
 
 open System.Threading.Tasks
 open Catalog.Api.Model
+open MongoDB.Driver
 
 type IProductRepository =
-    abstract member GetBySlug: slug: string -> Task<Product>
+    abstract member GetBySlug: slug: string -> Task<Product option>
+
+type Storage =
+    | MongoDb of database: IMongoDatabase
+
+module Product =
+
+  let storage = function
+    | MongoDb db ->
+      { new IProductRepository with
+          member __.GetBySlug slug =
+            Task.FromResult(None) }
