139a4aff00d177c77a3065502399d0e363d3e393
Dominik Kotecki <dominikus1910@gmail.com>
2018-07-16 20:05:05 +0200

    add Mongo serialization attributes

--- src/Services/Catalog/Catalog.Api/Model.fs
+++ src/Services/Catalog/Catalog.Api/Model.fs
@@ -1,10 +1,13 @@
-namespace Catalog.Api.Model 
+namespace Catalog.Api.Model
 
 open System
+open MongoDB.Bson.Serialization.Attributes
+open MongoDB.Bson
+
 [<CLIMutable>]
-type Product = { Id: Guid; 
-                 Slug: string;
-                 Name: string;
-                 Price: decimal
-                 AvailableStock: int
-                 PictureUri: string }
+type Product = { [<BsonId>] [<BsonRepresentation(BsonType.String)>] Id: Guid;
+                 [<BsonElement>] Slug: string;
+                 [<BsonElement>] Name: string;
+                 [<BsonElement>] Price: decimal
+                 [<BsonElement>] AvailableStock: int
+                 [<BsonElement>] PictureUri: string }
