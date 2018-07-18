ec38349ee3896b76e47e8e5b814f3c2ffcfd0d29
Dominik Kotecki <dominikus1910@gmail.com>
2018-07-18 18:50:39 +0200

    small changes

--- src/Services/Catalog/Catalog.Domain/Service.fs
+++ src/Services/Catalog/Catalog.Domain/Service.fs
@@ -1,1 +1,12 @@
 namespace Catalog.Api.Services
+
+open System.Threading.Tasks
+open Catalog.Api.Model
+open FSharp.Control.Tasks
+
+module Product =
+
+  let getBySlug (f: (string) -> Task<Product option> ) slug =
+    task {
+      return! f(slug)
+    }
