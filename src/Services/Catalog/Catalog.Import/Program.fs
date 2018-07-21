a9407bdcba9a62d12adda6dc8fcea8dd4619ffcd
Dominik Kotecki <dominikus1910@gmail.com>
2018-07-21 14:35:33 +0200

    add argu

--- src/Services/Catalog/Catalog.Import/Program.fs
+++ src/Services/Catalog/Catalog.Import/Program.fs
@@ -1,6 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Argu
+
+type Arguments =
+    | [<AltCommandLine("-c")>] ConnectionString of connstr:string
+    | [<AltCommandLine("-q")>] ProductsQuantity of q:int
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | ProductsQuantity _ -> "Specify a products quantity to generation"
+            | ConnectionString _ -> "Specify a mongodb connectionstring."
 
 [<EntryPoint>]
 let main argv =
