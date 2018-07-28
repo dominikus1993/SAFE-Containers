5fc80a8e7b50cd2fc2b992b088f40696710e7a5d
Dominik Kotecki <dominikus1910@gmail.com>
2018-07-28 19:23:12 +0200

    setup elmish

--- src/Web/WebSPA/App.fs
+++ src/Web/WebSPA/App.fs
@@ -3,5 +3,34 @@ module WebSPA
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
+open Elmish
 
-Browser.console.log(Urls.CatalogApiUrl)
+type Model =
+    { x : int }
+
+type Msg =
+    | Increment
+    | Decrement
+
+let init () =
+    { x = 0 }, Cmd.ofMsg Increment
+
+let update msg model =
+    match msg with
+    | Increment when model.x < 3 ->
+        { model with x = model.x + 1 }, Cmd.ofMsg Increment
+
+    | Increment ->
+        { model with x = model.x + 1 }, Cmd.ofMsg Decrement
+
+    | Decrement when model.x > 0 ->
+        { model with x = model.x - 1 }, Cmd.ofMsg Decrement
+
+    | Decrement ->
+        { model with x = model.x - 1 }, Cmd.ofMsg Increment
+
+let view =
+
+Program.mkProgram init update (fun model _ -> printf "%A\n" model)
+|> Program.withConsoleTrace
+|> Program.run
