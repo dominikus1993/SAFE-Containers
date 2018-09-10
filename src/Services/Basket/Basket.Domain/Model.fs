2202555cf3927bcf350b0d4a5f5c1d21726286ed
dominikus1993 <dominikus1910@gmail.com>
2018-09-10 20:16:51 +0200

    add tests form add item method

--- src/Services/Basket/Basket.Domain/Model.fs
+++ src/Services/Basket/Basket.Domain/Model.fs
@@ -15,6 +15,16 @@ module Aggregates =
   open Values
   open Entities
 
-  type CustomerBasket = { Id: Id; Items: CustomerBasketItem list; History: History } with
-    member this.AddItem(item: CustomerBasketItem) =
-      this
+  type CustomerBasket = { Id: Id; Items: CustomerBasketItem list; History: History }
+
+  module CustomerBasket =
+    let zero () =
+      { Id = Guid.NewGuid(); Items = []; History = { CreationTime = DateTime.UtcNow; LastUpdate = DateTime.UtcNow } }
+
+    let addItem (item: CustomerBasketItem) (basket: CustomerBasket) =
+      match basket.Items with
+      | [] -> { basket with Items = [item]; History = { basket.History with LastUpdate = DateTime.UtcNow } }
+      | list when list |> List.exists(fun x -> x.Id = item.Id ) ->
+        { basket with Items = list |> List.map(fun i -> if i.Id = item.Id then { i with Quantity = i.Quantity + item.Quantity } else i ) ; History = { basket.History with LastUpdate = DateTime.UtcNow } }
+      | list->
+        { basket with Items = item :: list ; History = { basket.History with LastUpdate = DateTime.UtcNow } }
--- src/Services/Basket/Basket.UnitTests/CustomerBasketModel.fs
+++ src/Services/Basket/Basket.UnitTests/CustomerBasketModel.fs
@@ -2,12 +2,26 @@ module CustomerBasketModel
 
 
 open Expecto
+open Basket.Domain.Model.Aggregates
+open Basket.Domain.Model.Entities
+open System
 
 [<Tests>]
 let tests =
   testList "CustomerBasket" [
     testList "AddItem" [
       testCase "Add item when basket is empty" <| fun _ ->
-        Expect.isTrue true ""
+        let uuid = Guid.NewGuid()
+        let subject = CustomerBasket.zero() |> CustomerBasket.addItem { Id = uuid; Quantity = 1 }
+        Expect.equal subject.Items ([{ Id = uuid; Quantity = 1 } ]) "Item should contain [{ Id = uuid; Quantity = 1 } ]"
+      testCase "Add item when basket is not empty" <| fun _ ->
+        let uuid = Guid.NewGuid()
+        let subject = CustomerBasket.zero() |> CustomerBasket.addItem { Id = uuid; Quantity = 1 }  |> CustomerBasket.addItem { Id = uuid; Quantity = 1 }
+        Expect.equal subject.Items ([{ Id = uuid; Quantity = 2 } ]) "Item should contain [{ Id = uuid; Quantity = 2 } ]"
+      testCase "Add two distinct items when basket is not empty" <| fun _ ->
+        let uuid = Guid.NewGuid()
+        let uuid2 = Guid.NewGuid()
+        let subject = CustomerBasket.zero() |> CustomerBasket.addItem { Id = uuid; Quantity = 1 }  |> CustomerBasket.addItem { Id = uuid2; Quantity = 1 }
+        Expect.equal subject.Items ([{ Id = uuid2; Quantity = 1 }; { Id = uuid; Quantity = 1 } ]) "Item should contain [{ Id = uuid; Quantity = 1 }; { Id = uuid2; Quantity = 1 } ]"
     ]
   ]
