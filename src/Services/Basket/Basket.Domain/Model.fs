08cd5ec93da384881f942aa8c4c481c6097bbd26
dominikus1993 <dominikus1910@gmail.com>
2018-09-13 22:50:17 +0200

    small change

--- src/Services/Basket/Basket.Domain/Model.fs
+++ src/Services/Basket/Basket.Domain/Model.fs
@@ -36,26 +36,17 @@ module Aggregates =
 
   type NoItems = NoItems
   type EmptyBasketState = NoItems
-  type ActiveBasketState = { CustomerData: CustomerData; Items: CustomerBasketItem list; History: History }
-
-  type CustomerBasket = { Id: Id; CustomerData: CustomerData; Items: CustomerBasketItem list; History: History }
+  type ActiveBasketState = { Items: CustomerBasketItem list; }
 
   type Basket =
-    | Empty of Id: Guid * EmptyBasketState
-    | LookingAround of Id: Guid * CustomerData: CustomerData * Items: CustomerBasketItem list * History: History
-    | Paid of CustomerBasket
+    | Empty of Id: Id * CustomerData: CustomerData * History: History * EmptyBasketState
+    | LookingAround of Id: Guid * CustomerData: CustomerData * Items: ActiveBasketState * History: History
+
+  type CustomerBasket = { Id: Id; CustomerData: CustomerData; Basket: Basket; History: History }
 
   module CustomerBasket =
     let zero (customerId) =
-      { Id = Guid.NewGuid(); CustomerData = { Id = customerId }; Items = []; History = { CreationTime = DateTime.UtcNow; LastUpdate = DateTime.UtcNow } }
-
-    let addItem (item: CustomerBasketItem) (basket: CustomerBasket): Result<CustomerBasket, Messages.DomainMessage> =
-      match basket.Items with
-      | [] -> Ok({ basket with Items = [item]; History = { basket.History with LastUpdate = DateTime.UtcNow } })
-      | list when list |> List.exists(fun x -> x.Id = item.Id ) ->
-        Ok({ basket with Items = list |> List.map(fun i -> if i.Id = item.Id then i |> CustomerBasketItem.incrementQuantity item.Quantity else i ) ; History = { basket.History with LastUpdate = DateTime.UtcNow } })
-      | list->
-        Ok({ basket with Items = item :: list ; History = { basket.History with LastUpdate = DateTime.UtcNow } })
+      { Id = Guid.NewGuid(); CustomerData = { Id = customerId }; Basket = Empty(NoItems) ; History = { CreationTime = DateTime.UtcNow; LastUpdate = DateTime.UtcNow } }
 
 
 
