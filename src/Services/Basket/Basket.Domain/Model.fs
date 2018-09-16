ae128d020275a52c417a5e0e0c7e7280fee99738
dominikus1993 <dominikus1910@gmail.com>
2018-09-16 12:54:36 +0200

    small change

--- src/Services/Basket/Basket.Domain/Model.fs
+++ src/Services/Basket/Basket.Domain/Model.fs
@@ -1,6 +1,5 @@
 namespace Basket.Domain.Model
 
-open Aggregates
 module Messages =
   type DomainMessage =
     | ElementNotExistsInBasket
@@ -9,12 +8,6 @@ module Values =
   open System
   type Id = Guid
   type History = { CreationTime: DateTime; LastUpdate: DateTime }
-  type State =
-
-    | New
-    | LookingAround
-    | Shopping
-    | Paid
 
 module Entities =
   open Values
@@ -34,19 +27,29 @@ module Aggregates =
   open Entities
   open System
 
-  type NoItems = NoItems
-  type EmptyBasketState = NoItems
-  type ActiveBasketState = { Items: CustomerBasketItem list; }
+  type EmptyState = NoItems
+  type ActiveState = { Items: CustomerBasketItem list }
 
-  type Basket =
-    | Empty of Id: Id * CustomerData: CustomerData * History: History * EmptyBasketState
-    | LookingAround of Id: Guid * CustomerData: CustomerData * Items: ActiveBasketState * History: History
+  type BasketState =
+    | Empty of EmptyState
+    | Active of ActiveState
 
-  type CustomerBasket = { Id: Id; CustomerData: CustomerData; Basket: Basket; History: History }
+  type CustomerBasket = { Id: Id; CustomerData: CustomerData; State: BasketState ; History: History }
 
   module CustomerBasket =
     let zero (customerId) =
-      { Id = Guid.NewGuid(); CustomerData = { Id = customerId }; Basket = Empty(NoItems) ; History = { CreationTime = DateTime.UtcNow; LastUpdate = DateTime.UtcNow } }
+      { Id = Guid.NewGuid(); CustomerData = { Id = customerId }; State = Empty(NoItems); History = { CreationTime = DateTime.UtcNow; LastUpdate = DateTime.UtcNow } }
+
+    let addItem (item: CustomerBasketItem) (basket: CustomerBasket) =
+      match basket.State with
+      | Empty(_) -> { basket with State = Active({ Items = [item] }); History = { basket.History with LastUpdate = DateTime.UtcNow } }
+      | Active(state)->
+        match state with
+        | { Items = list } when list |> List.exists(fun i -> i.Id = item.Id) ->
+          let newItemList = list |> List.map(fun i -> if i.Id = item.Id then { i with Quantity = i.Quantity + item.Quantity } else i )
+          { basket with State = Active({ Items = newItemList }); History = { basket.History with LastUpdate = DateTime.UtcNow } }
+        | { Items = list } ->
+          { basket with State = Active({ Items = item :: list  }); History = { basket.History with LastUpdate = DateTime.UtcNow } }
 
 
 
