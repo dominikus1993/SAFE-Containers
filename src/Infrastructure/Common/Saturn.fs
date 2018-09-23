2a9621527d7c6d1299b8530f42fe4038e22933b4
dominikus1993 <dominikus1910@gmail.com>
2018-09-23 22:30:26 +0200

    add AppMetrics

--- src/Infrastructure/Common/Saturn.fs
+++ src/Infrastructure/Common/Saturn.fs
@@ -1,9 +1,11 @@
 module Saturn
+open Microsoft.AspNetCore.Hosting
 open Saturn
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Http
 open System
-
+open App.Metrics.AspNetCore
+open App.Metrics.AspNetCore.Health
 type ApplicationBuilder with
   [<CustomOperation("use_pathbase")>]
   member __.UsePathBase(state: ApplicationState, path: string) =
@@ -12,3 +14,6 @@ type ApplicationBuilder with
     else
       { state with
           AppConfigs = (fun (app : IApplicationBuilder)-> app.UsePathBase(PathString.FromUriComponent(path)))::state.AppConfigs}
+  [<CustomOperation("use_app_metrics")>]
+  member __.UseAppMetrics(state: ApplicationState) =
+    { state with HostConfigs = (fun (builder: IWebHostBuilder) -> builder.UseMetrics().UseMetricsEndpoints().UseMetricsWebTracking().UseHealth().UseHealthEndpoints())::state.HostConfigs }
