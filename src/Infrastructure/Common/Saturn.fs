e7bb34f7525a126f4e8cb5b7454f8bafb4b45084
dominikus1993 <dominikus1910@gmail.com>
2018-09-24 21:13:12 +0200

    add metrics

--- src/Infrastructure/Common/Saturn.fs
+++ src/Infrastructure/Common/Saturn.fs
@@ -6,6 +6,14 @@ open Microsoft.AspNetCore.Http
 open System
 open App.Metrics.AspNetCore
 open App.Metrics.AspNetCore.Health
+open AppMetrics
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.DependencyInjection.Extensions
+open Microsoft.Extensions.DependencyInjection
+open App.Metrics.AspNetCore.Health.Endpoints
+open Microsoft.Extensions.Options
+open App.Metrics.AspNetCore.Endpoints
+
 type ApplicationBuilder with
   [<CustomOperation("use_pathbase")>]
   member __.UsePathBase(state: ApplicationState, path: string) =
@@ -15,5 +23,11 @@ type ApplicationBuilder with
       { state with
           AppConfigs = (fun (app : IApplicationBuilder)-> app.UsePathBase(PathString.FromUriComponent(path)))::state.AppConfigs}
   [<CustomOperation("use_app_metrics")>]
-  member __.UseAppMetrics(state: ApplicationState) =
-    { state with HostConfigs = (fun (builder: IWebHostBuilder) -> builder.UseMetrics().UseMetricsEndpoints().UseMetricsWebTracking().UseHealth().UseHealthEndpoints())::state.HostConfigs }
+  member __.UseAppMetrics(state: ApplicationState, pathBase: string option) =
+    { state with
+        HostConfigs = (fun (builder: IWebHostBuilder) -> builder.UseMetrics().UseMetricsEndpoints().UseMetricsWebTracking().UseHealth().UseHealthEndpoints())::state.HostConfigs
+        ServicesConfig = (fun (services: IServiceCollection) ->
+                                  let healtSD = ServiceDescriptor.Singleton<IConfigureOptions<HealthEndpointsHostingOptions>, ConfigureHealthHostingOptions>(fun _ -> ConfigureHealthHostingOptions(pathBase))
+                                  let metricsSD = ServiceDescriptor.Singleton<IConfigureOptions<MetricsEndpointsHostingOptions>, ConfigureMetricsHostingOptions>(fun _ -> ConfigureMetricsHostingOptions(pathBase))
+                                  services.TryAddEnumerable([| healtSD; metricsSD |])
+                                  services ) :: state.ServicesConfig }
