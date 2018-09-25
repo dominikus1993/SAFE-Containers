bfcca2df220411de49733638fab1e7931728bf5f
dominikus1993 <dominikus1910@gmail.com>
2018-09-25 21:45:45 +0200

    small changes

--- src/Services/Health/Health.App/Service.fs
+++ src/Services/Health/Health.App/Service.fs
@@ -1,2 +1,10 @@
 module Service
+open System.Collections.Generic
 
+[<CLIMutable>]
+type Check = { ServiceName: string; Url: string }
+
+[<CLIMutable>]
+type ServiceStatus = { Status: string; Healthy: IDictionary<string, string>; }
+
+let download urls = 2
