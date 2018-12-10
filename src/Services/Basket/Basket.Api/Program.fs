768ddb999c3810b0944fc028e9aa8d3ae7d31b55
dominikus1993 <dominikus1910@gmail.com>
2018-12-10 23:00:05 +0100

    add PathBase

--- src/Services/Basket/Basket.Api/Program.fs
+++ src/Services/Basket/Basket.Api/Program.fs
@@ -14,6 +14,10 @@ open System.Collections.Generic
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.Options
 open Microsoft.AspNetCore.Authentication.JwtBearer
+open Microsoft.IdentityModel.Tokens
+open System.Text
+open StackExchange.Redis
+open Basket.Domain.Storage
 
 
 let errorHandler (ex : Exception) (logger : ILogger) =
@@ -43,11 +47,26 @@ let configureApp (app : IApplicationBuilder) =
        .UseStaticFiles()
        .UseResponseCaching()
        .UseGiraffe webApp
+    let pathbase = Environment.getOrElse "PATH_BASE" ""
+    if String.IsNullOrEmpty(pathbase) |> not then
+      app.UsePathBase(PathString(pathbase)) |> ignore
 
 let configureServices (services : IServiceCollection) =
     services
         .AddResponseCaching()
         .AddGiraffe() |> ignore
+    services.AddAuthentication(fun opt ->
+                                opt.DefaultAuthenticateScheme <- JwtBearerDefaults.AuthenticationScheme
+                                opt.DefaultChallengeScheme <- JwtBearerDefaults.AuthenticationScheme
+                              )
+            .AddJwtBearer(fun cfg ->
+                            let issuer = configuration.["Service:Jwt:Issuer"]
+                            let secret = SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.["Service:Jwt:SecretKey"]))
+                            cfg.IncludeErrorDetails <- true
+                            cfg.TokenValidationParameters <- TokenValidationParameters(ValidateAudience = true, ValidateIssuer = true, ValidIssuer = issuer, ValidAudience = issuer, IssuerSigningKey = secret)
+                          ) |> ignore
+    services.AddSingleton<IConnectionMultiplexer>(fun opt -> ConnectionMultiplexer.Connect(configuration.["Service:Database:Connection"]) :> IConnectionMultiplexer) |> ignore
+    services.AddTransient<ICustomerBasketRepository>(fun opt -> CustomerBasket.storage (opt.GetService<IConnectionMultiplexer>())) |> ignore
 
 let configureLogging (loggerBuilder : ILoggingBuilder) =
     loggerBuilder.AddFilter(fun lvl -> lvl.Equals LogLevel.Error)
